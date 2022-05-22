13051032fde2cfb081f9a151a1167e11d35d2e1f
Foo Chert Yii <foochertyii394@gmail.com>
2022-05-22 11:57:58 +0800

    port

--- public/script.js
+++ public/script.js
@@ -1,24 +1,21 @@
-const socket = io('/');
-const videoGrid = document.getElementById('video-grid');
-console.log(videoGrid)
-const myVideo = document.createElement('video');
-myVideo.muted = true;
-
-var peer = new Peer(undefined, {
+cconst socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
   path: '/peerjs',
   host: '/',
   port: '443'
-});
-
+})
 let myVideoStream;
+const myVideo = document.createElement('video')
+myVideo.muted = true;
+const peers = {}
 navigator.mediaDevices.getUserMedia({
   video: true,
   audio: true
 }).then(stream => {
-  myVideoStream = stream; // the myvideostream is coming from here
-  addVideoStream(myVideo, stream);
-
-  peer.on('call', call => {
+  myVideoStream = stream;
+  addVideoStream(myVideo, stream)
+  myPeer.on('call', call => {
     call.answer(stream)
     const video = document.createElement('video')
     call.on('stream', userVideoStream => {
@@ -26,99 +23,62 @@ navigator.mediaDevices.getUserMedia({
     })
   })
 
-  socket.on('user-connected', (userId) => {
-    connectToNewUser(userId, stream);
+  socket.on('user-connected', userId => {
+    connectToNewUser(userId, stream)
   })
-  let text = $('input')
-
-  $('html').keydown((e) => {
-    if(e.which == 13 && text.val().length !== 0) {
-      socket.emit('message', text.val()); // This will send using .emit()
+  // input value
+  let text = $("input");
+  // when press enter send message
+  $('html').keydown(function (e) {
+    if (e.which == 13 && text.val().length !== 0) {
+      socket.emit('message', text.val());
       text.val('')
     }
-  })
-  socket.on('createMessage', message => {
-    console.log(message);
-    $('.messages').append(`<li class="message"><b>user</b><br/>${message}</li>`)
+  });
+  socket.on("createMessage", message => {
+    $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
     scrollToBottom()
   })
 })
 
-peer.on('open', id => {
-  socket.emit('join-room', ROOM_ID, id);
+socket.on('user-disconnected', userId => {
+  if (peers[userId]) peers[userId].close()
 })
 
-const connectToNewUser = (userId, stream) => {
-  const call = peer.call(userId, stream)
+myPeer.on('open', id => {
+  socket.emit('join-room', ROOM_ID, id)
+})
+
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
     addVideoStream(video, userVideoStream)
   })
+  call.on('close', () => {
+    video.remove()
+  })
+
+  peers[userId] = call
 }
 
-const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
+function addVideoStream(video, stream) {
+  video.srcObject = stream
   video.addEventListener('loadedmetadata', () => {
-    video.play();
+    video.play()
   })
-  videoGrid.append(video);
+  videoGrid.append(video)
 }
+
+
+
 const scrollToBottom = () => {
-  let d = $('.main__chat__window');
+  var d = $('.main__chat_window');
   d.scrollTop(d.prop("scrollHeight"));
 }
 
-const setMuteButton = () => {
-  const html = `
-  <i class="fas fa-microphone"></i>
-  <span>Mute</span>
-  `
 
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
-const setUnmuteButton = () => {
-  const html = `
-  <i class="unmute fas fa-microphone-slash"></i>
-  <span>Unmute</span>
-  `
-
-  document.querySelector('.main__mute_button').innerHTML = html;
-}
-
- const playStop = () => {
-   let enabled = myVideoStream.getVideoTracks()[0].enabled;
-   if (enabled) {
-     myVideoStream.getVideoTracks()[0].enabled = false;
-     setPlayVideo();
-
-   } else {
-     setStopVideo()
-     myVideoStream.getVideoTracks()[0].enabled = true;
-   }
- }
-
- const setStopVideo = () => {
-   const html = `
-   <i class="fas fa-video"></i>
-   <span>Stop Video</span>
-   `
-
-   document.querySelector('.main__video_button').innerHTML = html;
- }
-
- const setPlayVideo = () => {
-   const html = `
-   <i class="stop fas fa-video-slash"></i>
-   <span>Play Video</span>`
-
-   document.querySelector('.main__video_button').innerHTML = html;
- }
-
-
-// This will mute and unmute when click
 const muteUnmute = () => {
-  console.log(myVideoStream)
   const enabled = myVideoStream.getAudioTracks()[0].enabled;
   if (enabled) {
     myVideoStream.getAudioTracks()[0].enabled = false;
@@ -129,5 +89,48 @@ const muteUnmute = () => {
   }
 }
 
+const playStop = () => {
+  console.log('object')
+  let enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo()
+  } else {
+    setStopVideo()
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+}
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `
+  document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `
+  document.querySelector('.main__video_button').innerHTML = html;
+}
 
 // WebRTC, an open source project that provides web browsers and mobile applocations with real-time communication via simple application programming interfaces
--- server.js
+++ server.js
@@ -1,39 +1,42 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
+const express = require('express')
+const app = express()
+// const cors = require('cors')
+// app.use(cors())
+const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { v4: uuidv4 } = require('uuid');
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
   debug: true
-}) // we have imported a peer Js and we have ask ExpressPeerServer to connect
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+});
+const { v4: uuidV4 } = require('uuid')
 
 app.use('/peerjs', peerServer);
-app.get('/', (req, res) => { // This ig oing to be a room, this is a paramter
-  res.redirect(`/${uuidv4()}`);
 
-  // So this will automatically generate a uuid (a number) for your room when you create a new room
-  // the upload get will get the uuid function that generated it, and we are going to pass the const to the front
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.redirect(`/${uuidV4()}`)
 })
-
-// So the uuid that has been generated redirects me to the room call roomId
 
 app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room }) // This says what view files that we're going to render
+  res.render('room', { roomId: req.params.room })
 })
-
-
-
 
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.broadcast.to(roomId).emit('user-connected', userId);
-    socket.on('message', message => {
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+    // messages
+    socket.on('message', (message) => {
+      //send message to the same room
       io.to(roomId).emit('createMessage', message)
+  });
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId)
     })
   })
 })
-server.listen(process.env.PORT||3030);
+
+server.listen(process.env.PORT||3030)
